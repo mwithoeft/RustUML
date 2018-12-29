e0c4ac5d5d77469163f258fda9775f6bf11a6b05
mwithoeft <moritz_pit.withoeft@fh-bielefeld.de>
2018-12-29 14:28:14 +0100

    Beispieleingaben hinzugefügt

---
+++ rust_uml/src/examples.rs
@@ -0,0 +1,59 @@
+/*Beispieleingaben*/
+pub static CLASS_EXAMPLE: &str = "type \"classdiagram\"
+
+classname \"Kunde\"
+attribute \"+, kundenNr, String\"
+
+classname \"Einkauf\"
+attribute \"+, datum, Date\"
+attribute \"+, bestellID, String\"
+method \"+, bestellen, boolean\"
+method \"+, artikelHinzufuegen, void, artikel:Artikel\"
+
+classname \"Lagerbestand\"
+keywords \"interface\"
+method \"#, istAuslieferbar, boolean\"
+method \"#, anzahl, integer, Object\"
+
+classname \"Artikel\"
+attribute \"-, bezeichnung, String\"
+attribute \"-, preis, integer\"
+method \"#, istAuslieferbar, boolean\"
+method \"#, anzahl, integer, Artikel\"
+
+classname \"Kategorie\"
+attribute \"-, bezeichnung, String\"
+
+classname \"Person\"
+property \"abstract\"
+attribute \"-, name, String\"
+attribute \"-, vorname, String\"
+
+relationship \"association, Kunde, false, 1, Einkauf, false, 0..*\"
+relationship \"extends, Kunde, false, -, Person, true, -\"
+relationship \"aggregation, Einkauf, true, 0..*, Artikel, false, 1..*\"
+relationship \"implements, Artikel, false, -, Lagerbestand, true, -\"
+relationship \"aggregation, Kategorie, true, 1, Artikel, false, 1..*\"
+";
+
+
+pub static USECASE_EXAMPLE: &str = "type \"usecase diagram\"
+
+elementtype \"subject, Multimediasystem\"
+
+elementtype \"actor, Benutzer1\"
+elementtype \"actor, Benutzer2\"
+elementtype \"usecase, CD-brennen\"
+elementtype \"usecase, CD-erstellen\"
+elementtype \"usecase, CD-beschriften\"
+elementtype \"usecase, Booklet-erstellen\"
+elementtype \"extpoint, Beschriftung, CD-erstellen\"
+
+relationship \"association, Benutzer1, -, CD-erstellen, -\"
+relationship \"association, Benutzer1, -, CD-brennen, -\"
+relationship \"association, Benutzer1, -, Booklet-erstellen, -\"
+relationship \"association, Benutzer2, -, CD-beschriften, -\"
+relationship \"include, CD-erstellen, -, CD-brennen, -\"
+relationship \"include, CD-erstellen, -, Booklet-erstellen, -\"
+relationship \"extend, CD-beschriften, -, CD-erstellen, -\"
+";
--- rust_uml/src/lib.rs
+++ rust_uml/src/lib.rs
@@ -14,6 +14,7 @@ mod build_class_svg;
 mod parsing;
 mod svglib;
 mod get_diagram_type;
+mod examples;
 
 
 pub struct Model {
@@ -43,17 +44,19 @@ impl Component for Model {
             }
 
             Msg::SelectType => {
-                /*Beispieldaten*/
                 let s : String = get_type();
-                if s == "class" {}
-                else if s == "usecase" {}
+                if s == "class" {insert_at_caret("eingabefeld", examples::CLASS_EXAMPLE);}
+                else if s == "usecase" {insert_at_caret("eingabefeld", examples::USECASE_EXAMPLE);}
                 else if s == "action" {}
                 else if s == "sequence" {}
                 else if s == "state" {}
                 else if s == "component" {}
                 else if s == "package" {}
                 else if s == "deployment" {}
-                else if s == "object" {}           
+                else if s == "object" {}
+                js!(document.getElementById("selectBox").selectedIndex = "0";);
+                self.text_area = (js!{return document.getElementById("eingabefeld").value}).into_string().unwrap();
+                work_on_input(&self.text_area);
             }
 
         }
@@ -70,11 +73,13 @@ impl Renderable<Model> for Model {
                     <textarea rows=5,
                         value=&self.text_area,
                         oninput=|e| Msg::GotInput(e.value),
-                        placeholder="Hier können Sie ihre Spezifikation eintragen",>
+                        placeholder="Hier können Sie ihre Spezifikation eintragen",
+                        id="eingabefeld",>
                     </textarea>
                 </div>
                 <div id="buttons",>
                     <select id="selectBox", onchange=|_| Msg::SelectType,>
+                        <option value="", disabled=true, required=true,>{"Beispieleingaben:"}</option>
                         <option value="class",>{"Klassendiagramm"}</option>
                         <option value="usecase",>{"Anwendungsfalldiagramm"}</option>
                         <option value="action",>{"Aktivitätsdiagramm"}</option>
@@ -121,104 +126,12 @@ fn work_on_input(input: &str) {
 
         match diagramm._typ {
             get_diagram_type::DiagramType::CLASS => {
-                    
+                js!(console.log("Parse Class!"));     
                 let mut klassen: Vec<parsing::parse_class::Klasse> = Vec::new();
                 let mut beziehungen_class: Vec<parsing::parse_class::Beziehung> = Vec::new();
-
-                js!(console.log("Parse Class!"));  
                 parsing::parse_class::parse(&diagramm._input, &mut klassen, &mut beziehungen_class);
-
                 let mut svg : String = build_class_svg::build_klassendiagramm(&mut klassen, &mut beziehungen_class).to_string();
-                
-
-                js!{
-                    var panOptions = {
-                        viewportSelector: ".svg-pan-zoom_viewport",
-                        panEnabled: true, 
-                        controlIconsEnabled: false, 
-                        zoomEnabled: true, 
-                        dblClickZoomEnabled: true, 
-                        mouseWheelZoomEnabled: true, 
-                        preventMouseEventsDefault: true, 
-                        zoomScaleSensitivity: 0.2, 
-                        minZoom: 0.5, 
-                        maxZoom: 10,
-                        fit: false, 
-                        contain: true,
-                        center: false, 
-                        refreshRate: "auto"
-                    };
-
-                    var button = document.createElement("button");
-                    button.classList.add("tablinks");
-                    if (@{number.to_string()} == @{&counter.to_string()}) {
-                        button.classList.add("active");
-                    }
-
-                    button.onclick = function(){
-
-                        var i, tabcontent, tablinks;
-                        // Hole alle Elemente mit der Klasse "tabcontent" and verstecke sie, entferne panzoom
-                        tabcontent = document.getElementsByClassName("tabcontent");
-                        for (i = 0; i < tabcontent.length; i++) {
-                            if (i == document.getElementById("bild").dataset.counter) {
-                                svgPanZoom(tabcontent[i].firstChild).destroy();
-                                tabcontent[i].style.display = "none";
-                                var newParent = tabcontent[i].firstChild;
-                                var oldParent = newParent.firstChild;
-                                while (oldParent.childNodes.length > 0) {
-                                    newParent.appendChild(oldParent.childNodes[0]);
-                                }
-                            }
-
-                        }
-
-                        /*Entferne den alten pan-viewport*/
-                        var vp = document.getElementsByClassName("svg-pan-zoom_viewport");
-                        while(vp[0]) {
-                            vp[0].parentNode.removeChild(vp[0]);
-                        }
-
-                        //Hole alle Elemente mit der Klasse "tablinks" und entferne die Klasse "active"
-                        var p = 0;
-                        tablinks = document.getElementsByClassName("tablinks");
-                        for (i = 0; i < tablinks.length; i++) {
-                            tablinks[i].className = tablinks[i].className.replace(" active", "");
-                            if (tablinks[i] == this) {
-                                this.classList.add("active");
-                                p = i;
-                                var tmp = document.getElementById("bild");
-                                tmp.dataset.counter = i;
-                            }
-                        }
-                        var tc = document.getElementsByClassName("tabcontent");
-                        for (i = 0; i <= p; i++) {
-                            if (i == p) {
-                                tc[i].style.display = "block";
-                                tc[i].firstChild.setAttribute("viewBox", "0 0 1680 720");
-                                svgPanZoom(tc[i].firstChild, panOptions);
-                            }
-                        }
-                    };
-
-                    button.value = "d_" + @{(number+1).to_string()};
-                    button.innerHTML = "Klassendiagramm " + @{(number+1).to_string()};
-                    var tab = document.getElementsByClassName("tab");
-                    for (var i = 0; i < tab.length; i++) {
-                        tab[i].appendChild(button);
-                    }
-
-                    var tabcontent = document.createElement("div");
-                    tabcontent.classList.add("tabcontent");
-                    tabcontent.id = "d_" + @{(number+1).to_string()};
-                    tabcontent.innerHTML = @{svg};
-                    document.getElementById("bild").appendChild(tabcontent);
-
-                    if (@{number.to_string()} == @{&counter.to_string()}) {
-                        tabcontent.style.display = "block";
-                        svgPanZoom(tabcontent.firstChild, panOptions);
-                    }
-                }
+                make_tabs(number, counter, svg);
             }
 
             get_diagram_type::DiagramType::USECASE => {
@@ -242,5 +155,132 @@ fn work_on_input(input: &str) {
             get_diagram_type::DiagramType::NOTFOUND => { js!(console.log("NOTFOUND!"));  }
         }
 
+    }
+}
+
+fn make_tabs(number: usize, counter: String, svg: String) {
+    js!{
+        var panOptions = {
+            viewportSelector: ".svg-pan-zoom_viewport",
+            panEnabled: true, 
+            controlIconsEnabled: false, 
+            zoomEnabled: true, 
+            dblClickZoomEnabled: true, 
+            mouseWheelZoomEnabled: true, 
+            preventMouseEventsDefault: true, 
+            zoomScaleSensitivity: 0.2, 
+            minZoom: 0.5, 
+            maxZoom: 10,
+            fit: false, 
+            contain: true,
+            center: false, 
+            refreshRate: "auto"
+        };
+
+        var button = document.createElement("button");
+        button.classList.add("tablinks");
+        if (@{number.to_string()} == @{&counter.to_string()}) {
+            button.classList.add("active");
+        }
+
+        button.onclick = function(){
+
+            var i, tabcontent, tablinks;
+            // Hole alle Elemente mit der Klasse "tabcontent" and verstecke sie, entferne panzoom
+            tabcontent = document.getElementsByClassName("tabcontent");
+            for (i = 0; i < tabcontent.length; i++) {
+                if (i == document.getElementById("bild").dataset.counter) {
+                    svgPanZoom(tabcontent[i].firstChild).destroy();
+                    tabcontent[i].style.display = "none";
+                    var newParent = tabcontent[i].firstChild;
+                    var oldParent = newParent.firstChild;
+                    while (oldParent.childNodes.length > 0) {
+                        newParent.appendChild(oldParent.childNodes[0]);
+                    }
+                }
+
+            }
+
+            /*Entferne den alten pan-viewport*/
+            var vp = document.getElementsByClassName("svg-pan-zoom_viewport");
+            while(vp[0]) {
+                vp[0].parentNode.removeChild(vp[0]);
+            }
+
+            //Hole alle Elemente mit der Klasse "tablinks" und entferne die Klasse "active"
+            var p = 0;
+            tablinks = document.getElementsByClassName("tablinks");
+            for (i = 0; i < tablinks.length; i++) {
+                tablinks[i].className = tablinks[i].className.replace(" active", "");
+                if (tablinks[i] == this) {
+                    this.classList.add("active");
+                    p = i;
+                    var tmp = document.getElementById("bild");
+                    tmp.dataset.counter = i;
+                }
+            }
+            var tc = document.getElementsByClassName("tabcontent");
+            for (i = 0; i <= p; i++) {
+                if (i == p) {
+                    tc[i].style.display = "block";
+                    tc[i].firstChild.setAttribute("viewBox", "0 0 1680 720");
+                    svgPanZoom(tc[i].firstChild, panOptions);
+                }
+            }
+        };
+
+        button.value = "d_" + @{(number+1).to_string()};
+        button.innerHTML = "Klassendiagramm " + @{(number+1).to_string()};
+        var tab = document.getElementsByClassName("tab");
+        for (var i = 0; i < tab.length; i++) {
+            tab[i].appendChild(button);
+        }
+
+        var tabcontent = document.createElement("div");
+        tabcontent.classList.add("tabcontent");
+        tabcontent.id = "d_" + @{(number+1).to_string()};
+        tabcontent.innerHTML = @{svg};
+        document.getElementById("bild").appendChild(tabcontent);
+
+        if (@{number.to_string()} == @{&counter.to_string()}) {
+            tabcontent.style.display = "block";
+            svgPanZoom(tabcontent.firstChild, panOptions);
+        }
+    }
+}
+
+fn insert_at_caret(area_id: &str, text: &str) {
+    js!{
+        var txtarea = document.getElementById(@{area_id});
+		var scrollPos = txtarea.scrollTop;
+		var strPos = 0;
+		var br = ((txtarea.selectionStart || txtarea.selectionStart == '0') ? 
+			"ff" : (document.selection ? "ie" : false ) );
+		if (br == "ie") { 
+			txtarea.focus();
+			var range = document.selection.createRange();
+			range.moveStart ("character", -txtarea.value.length);
+			strPos = range.text.length;
+		}
+		else if (br == "ff") strPos = txtarea.selectionStart;
+	
+		var front = (txtarea.value).substring(0,strPos);  
+		var back = (txtarea.value).substring(strPos,txtarea.value.length); 
+		txtarea.value=front+@{text}+back;
+		strPos = strPos + @{text}.length;
+		if (br == "ie") { 
+			txtarea.focus();
+			var range = document.selection.createRange();
+			range.moveStart ("character", -txtarea.value.length);
+			range.moveStart ("character", strPos);
+			range.moveEnd ("character", 0);
+			range.select();
+		}
+		else if (br == "ff") {
+			txtarea.selectionStart = strPos;
+			txtarea.selectionEnd = strPos;
+			txtarea.focus();
+		}
+		txtarea.scrollTop = scrollPos;
     }
 }
